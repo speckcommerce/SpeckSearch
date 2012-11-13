2a9092e8455e77ec273aa583ce25f8c2cdbb352a
nigel lundsten <nigel. lundsten@gmail.com>
2012-11-13 09:18:47 -0700

    add files

---
+++ config/module.config.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'controllers' => array(
+        'invokables' => array(
+        ),
+    ),
+    'service_manager' => array(
+        'shared' => array(
+        ),
+        'invokables' => array(
+        ),
+    ),
+);
---
+++ module.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SpeckSearch;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        $config = array();
+        $configFiles = array(
+            __DIR__ . '/config/module.config.php',
+        );
+        foreach($configFiles as $configFile) {
+            $config = \Zend\Stdlib\ArrayUtils::merge($config, include $configFile);
+        }
+        return $config;
+    }
+}
